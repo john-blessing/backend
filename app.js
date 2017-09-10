91f7cee25a438e788ffdd7559123dcf4cea72d71
john <1585185302@qq.com>
2017-09-10 10:47:22 +0800

    commit

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.all("*", function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Authorization, Cookie, Accept, X-Requested-With");
     res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Credentials", true);
     res.header("X-Powered-By", ' 3.2.1');
     next();
 });
